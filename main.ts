8e545f2adc6f071afeb768c5e334d25c0e28738c
Kyphong Pham <130786963+Kyphong-Pham@users.noreply.github.com>
2025-10-06 13:18:15 -0400

    Formatted Code

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 /* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+*
+* Created by: Kyphong Pham
+* Created on: Sep 2025
+* This program below finds the perimeter and area of a rectangle
 */
 
-basic.showString('Hello, World!')
+basic.showString("A rectangle has dimensions 5 cm & 3 cm.")
+basic.showString("Perimeter: 2(5 + 3) = " + (2 * (5 + 3)) + " cm")
+basic.showString("Area: 5 x 3 = " + (5 * 3) + " cm")
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
